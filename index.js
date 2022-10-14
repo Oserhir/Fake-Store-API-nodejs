6b7875762d38767a5fafb68fc2ff3b317f54477e
OSERHIRDEV <oserhir10@gmail.com>
2022-10-14 21:06:05 +0100

    initialize a project

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ require("dotenv").config(); // acceder les variable environnement
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
