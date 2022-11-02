c91f21332166567d6b91003072b4c7ee304bfefb
OSERHIRDEV <oserhir10@gmail.com>
2022-11-02 13:35:39 +0100

    create Product Schema & Model

---
+++ Back-End/models/productSchema.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+// Create Schema
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      maxlength: 150,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      maxlength: 2000,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+    },
+    photo: {
+      type: Buffer,
+      contentType: String,
+    },
+    images: [String],
+    category: {
+      type: ObjectId,
+      ref: "Category",
+      require: true,
+    },
+    shipping: {
+      type: Boolean,
+      require: false,
+      default: false,
+    },
+    /*  priceAfterDiscount: {
+      type: Number,
+    }, 
+    color: {
+      type: [String],
+    }, 
+     brand: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Brand',
+    },
+    ratingsAverage: {
+      type: Number,
+      min: [1, 'Rating must be above or equal 1.0'],
+      max: [5, 'Rating must be below or equal 5.0'],
+      // set: (val) => Math.round(val * 10) / 10, // 3.3333 * 10 => 33.333 => 33 => 3.3
+    },
+        ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    */
+  },
+  { timestamps: true }
+);
+
+// Create model
+const ProductModel = mongoose.model("Product", productSchema);
+module.exports = ProductModel;
