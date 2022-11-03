3cf60c29ed433077dfd08352d57940a623cc39ff
OSERHIRDEV <oserhir10@gmail.com>
2022-11-03 12:57:37 +0100

    validate product data

--- Back-End/controllers/productController.js
+++ Back-End/controllers/productController.js
@@ -1,13 +1,35 @@
 const productModel = require("../models/productSchema");
+const Joi = require("joi");
 
 exports.createProduct = (req, res) => {
-  const { name, description, price, quantity, category } = req.body;
+  // Joi Validation
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    description: Joi.string().required(),
+    price: Joi.number().required(),
+    quantity: Joi.number().required(),
+    category: Joi.string().required(),
+  });
 
-  const image = req.file.path;
+  const { error, value } = schema.validate(req.body);
 
+  if (error) {
+    res.status(400).json({ error: error.details[0].message });
+  }
+  // image not Found
   if (req.file == null) {
     res.status(400).json({ error: "image could not upload" });
   }
+
+  // image should be less than 3MB in size
+  if (req.file.size > Math.pow(10, 6) * 3) {
+    return res
+      .status(400)
+      .json({ error: "File size exceeds the allowable limit of(3MB)" });
+  }
+
+  const { name, description, price, quantity, category } = value;
+  const image = req.file.path;
 
   productModel
     .create({ name, description, price, quantity, image, category })
