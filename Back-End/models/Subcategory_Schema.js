08fee6229bbc7482aad19a6f395e12dd0ab45592
OSERHIRDEV <oserhir10@gmail.com>
2022-11-07 20:16:42 +0100

    Use ESLint with Some Airbnb Configurations

---
+++ Back-End/models/Subcategory_Schema.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+// Create Schema
+const subcategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      minlength: 3,
+      maxlength: 32,
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+    },
+    category: {
+      type: ObjectId,
+      ref: "Category",
+      require: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// Create model
+const subcategoryModel = mongoose.model("Subcategory", subcategorySchema);
+module.exports = subcategoryModel;
