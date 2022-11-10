2aebcd283b7962cc927e04288132dc4265443e0c
OSERHIRDEV <oserhir10@gmail.com>
2022-11-10 16:47:07 +0100

    fix err

--- Back-End/controllers/productController.js
+++ Back-End/controllers/productController.js
@@ -7,161 +7,40 @@ const slugify = require("slugify");
 
 const ObjectId = require("mongoose").Types.ObjectId;
 
-//  desc create Product
-//  route POST /api/products/create/:userId
-//  access Private
+//  @desc create Product // Done
 exports.createProduct = (req, res) => {
-  // Joi Validation
-  const schema = Joi.object({
-    title: Joi.string().min(3).required(),
-    description: Joi.string().min(20).max(2000).required(),
-    quantity: Joi.number().required(),
-    sold: Joi.number(),
-    priceAfterDiscount: Joi.number().custom((value, helpers) => {
-      if (value >= req.body.price) {
-        throw new Error("priceAfterDiscount must be lower than price");
-      }
-      // Return the value unchanged
-      return value;
-    }),
-    colors: Joi.array(),
-    imageCover: Joi.string().required(),
-    images: Joi.array(),
-    price: Joi.number().max(20000).required(),
-    category: Joi.string()
-      .required()
-      .custom((id, helpers) => {
-        // check if the id is valid MongoDB ObjectId
-        if (!isValidObjectId(id)) {
-          return helpers.error(`${id} Not a valid ObjectId!'`);
-          // throw new Error(`${id} Not a valid ObjectId!`); // Cas 1
-        }
-        // Validate Category Existence in Our DB
-        categoryModel
-          .findById(id)
-          .then((category) => {
-            if (!category) {
-              // (`${id} Category Not Found!`); // Cas 1
-              return Promise.reject(new Error("Category Not Found"));
-            }
-          })
-          .catch((err) => {
-            console.log(err);
-          });
+  req.body.slug = slugify(req.body.title);
 
-        // Return the value unchanged
-        return id;
-      }),
-
-    subcategories: Joi.array(),
-    // .custom((subCategoriesId) => {
-    //   // check if the id is valid MongoDB ObjectId
-    //   for (let i = 0; i < subCategoriesId.length; i++) {
-    //     if (!isValidObjectId(subCategoriesId[i])) {
-    //       res
-    //         .status(404)
-    //         .json({ err: `${subCategoriesId[i]} Not a valid ObjectId!'` });
-    //     }
-    //   }
-    //   // Validate Subcategories Existence in Our DB
-    //   subCategoryModel
-    //     .find({ _id: { $exists: true, $in: subCategoriesId } })
-    //     .then((result) => {
-    //       if (result.length != subCategoriesId.length || result.length < 1) {
-    //         res.json({ err: "Invalid subCategories Id" });
-    //         //res.status(404).json({ err: `Invalid subCategories Id` });
-    //       }
-    //     })
-    //     .catch((err) => {
-    //       console.log(err);
-    //     });
-    //   // Return the value unchanged
-    //   return subCategoriesId;
-    // })
-    // .custom((value) => {
-    //   // Validate That Subcategories Belong to Category
-    //   subCategoryModel
-    //     .find({
-    //       category: req.body.category,
-    //     })
-    //     .then((subcategories) => {
-    //       const subCategoriesInDB = [];
-    //       subcategories.forEach((sb) => {
-    //         subCategoriesInDB.push(sb._id.toString());
-    //       });
-    //       // check if subategories ids in  db include subcategories in req.body
-    //       const checker = (target, arr) =>
-    //         target.every((v) => arr.includes(v));
-
-    //       if (!checker(value, subCategoriesInDB)) {
-    //         return Promise.reject(
-    //           new Error("subCategories not belong to category")
-    //         );
-    //       }
-    //     });
-    // }),
-    ////////////////////////////
-    brand: Joi.string(),
-    ratingsAverage: Joi.number().min(1).max(5),
-    ratingsQuantity: Joi.number(),
-  });
-
-  const { error, value } = schema.validate(req.body);
-
-  if (error) {
-    console.log({ err: error });
-    res.status(400).json({ error: error.details[0].message });
-  } else {
-    console.log({ Finish: value });
-  }
-
-  // productModel
-  //   .create(value)
-  //   .then((product) => {
-  //     res.status(201).json({ data: product });
-  //   })
-  //   .catch((err) => {
-  //     res.status(400).json(err.message);
-  //   });
+  productModel
+    .create(req.body)
+    .then((product) => {
+      res.status(201).json({ data: product });
+    })
+    .catch((err) => {
+      res.status(400).json(err.message);
+    });
 };
 
-//  desc update Product
-//  route PUT /api/products/:productId/:userId
-//  access Private
+//  @desc update Product  // Done
 exports.updateProduct = (req, res) => {
-  // Joi Validation
-  const schema = Joi.object({
-    title: Joi.string().required(),
-    description: Joi.string().required(),
-    quantity: Joi.number().required(),
-    price: Joi.number().required(),
-    category: Joi.string().required(),
-  });
+  const { productId } = req.params; //  const { id } = req.product._id;
+  req.body.slug = slugify(req.body.title);
 
-  const { error, value } = schema.validate(req.body);
-  value.slug = slugify(value.title);
+  productModel.findOneAndUpdate(
+    { _id: productId },
+    req.body,
+    { new: true },
+    (err, doc) => {
+      if (err) {
+        res.status(400).json({ err: "Something wrong when updating data!" });
+      }
 
-  if (error) {
-    res.status(400).json({ error: error.details[0].message });
-  }
-
-  let product = req.product;
-  product = _.extend(product, value);
-
-  product.save((err, product) => {
-    if (err) {
-      res.status(400).json({
-        error: "Product update failed",
-      });
+      res.json({ data: doc });
     }
-
-    res.json({ data: product });
-  });
+  );
 };
 
-//  desc Delete Product
-//  route DELETE /api/products/:productId/:userId
-//  access Private
+//  @desc Delete Product
 exports.removeProduct = (req, res) => {
   let product = req.product;
   product.remove((err, product) => {
@@ -173,6 +52,7 @@ exports.removeProduct = (req, res) => {
   res.status(204).json({});
 };
 
+//
 exports.productById = (req, res, next, id) => {
   productModel.findById(id).exec((err, product) => {
     if (err || !product) {
@@ -186,19 +66,15 @@ exports.productById = (req, res, next, id) => {
   });
 };
 
-//  desc Get  Product
-//  route GET /api/products/:productId
-//  access Public
-exports.showProduct = (req, res) => {
+//  @desc Get  Product
+exports.getProduct = (req, res) => {
   res.json({
     data: req.product,
   });
 };
 
-//  desc Get List of Products
-//  route GET /api/products
-//  access Public
-exports.allProducts = (req, res) => {
+//  @desc Get List of Products
+exports.getProducts = (req, res) => {
   let sortedBy = req.query.sortedBy ? req.query.sortedBy : "_id";
   let order = req.query.order ? req.query.order : "asc";
   let limit = req.query.limit ? parseInt(req.query.limit) : 6;
@@ -220,8 +96,6 @@ exports.allProducts = (req, res) => {
 };
 
 //  desc Get  Related Product
-//  route GET /api/products/related/:productId/
-//  access Public
 exports.listRelated = (req, res) => {
   let limit = req.query.limit ? parseInt(req.query.limit) : 6;
 
--- Back-End/router/products.js
+++ Back-End/router/products.js
@@ -3,80 +3,82 @@ const router = express.Router();
 const {
   searchProduct,
   listRelated,
-  allProducts,
+  getProducts,
   createProduct,
-  showProduct,
+  getProduct,
   productById,
   removeProduct,
   updateProduct,
 } = require("../controllers/productController");
+
 const { requireSignIn, isAuth, isAdmin } = require("../middlewares/auth");
 const { userById } = require("../middlewares/user");
-const multer = require("multer");
 
-//Configuration for Multer
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public/images");
-  },
-  filename: function (req, file, cb) {
-    //cb(null, `${file.fieldname}-${Date.now()}${getExt(file.mimetype)}`);
-    cb(
-      null,
-      `${joinString(
-        noSpecialChars(file.originalname),
-        "-"
-      )}-${Date.now()}${getExt(file.mimetype)}`
-    );
-  },
-});
-const upload = multer({ storage: storage });
-const getExt = (mimetype) => {
-  switch (mimetype) {
-    case "image/png":
-      return ".png";
-    case "image/jpeg":
-      return ".jpg";
-  }
-};
-const noSpecialChars = (str) => {
-  return str.replace(/[^a-zA-Z0-9 ]/g, "");
-};
-const joinString = (str, char) => {
-  return str.split(" ").join(char);
-};
+// const multer = require("multer");
 
-// Get all products
-router.get("/", allProducts);
-
-// Get related products i
-router.get("/related/:productId/", listRelated);
+// //Configuration for Multer
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./public/images");
+//   },
+//   filename: function (req, file, cb) {
+//     //cb(null, `${file.fieldname}-${Date.now()}${getExt(file.mimetype)}`);
+//     cb(
+//       null,
+//       `${joinString(
+//         noSpecialChars(file.originalname),
+//         "-"
+//       )}-${Date.now()}${getExt(file.mimetype)}`
+//     );
+//   },
+// });
+// const upload = multer({ storage: storage });
+// const getExt = (mimetype) => {
+//   switch (mimetype) {
+//     case "image/png":
+//       return ".png";
+//     case "image/jpeg":
+//       return ".jpg";
+//   }
+// };
+// const noSpecialChars = (str) => {
+//   return str.replace(/[^a-zA-Z0-9 ]/g, "");
+// };
+// const joinString = (str, char) => {
+//   return str.split(" ").join(char);
+// };
 
 // Add new product
 router.post(
   "/create/:userId",
-  upload.single("image"),
+  // upload.single("image"),
   [requireSignIn, isAuth, isAdmin],
   createProduct
 );
 
-// Get a single product
-router.get("/:productId/", showProduct);
+// Update a product
+router.put(
+  "/:productId/:userId",
+  // upload.single("image"),
+  [requireSignIn, isAuth, isAdmin],
+  updateProduct
+);
 
-// Delete a product
+// Delete a product 
 router.delete(
   "/:productId/:userId",
   [requireSignIn, isAuth, isAdmin],
   removeProduct
 );
 
-// Update a product
-router.put(
-  "/:productId/:userId",
-  upload.single("image"),
-  [requireSignIn, isAuth, isAdmin],
-  updateProduct
-);
+// Get all products // Done
+router.get("/", getProducts);
+
+// Get a single product // Done
+router.get("/:productId/", getProduct);
+
+// Get related products // Done
+router.get("/related/:productId/", listRelated);
 
 // Product Search
 router.post("/search", searchProduct);
