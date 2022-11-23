9f4869d6215133e955b852e7daffcb135d21506f
OSERHIRDEV <oserhir10@gmail.com>
2022-11-23 13:55:40 +0100

    Coupon Schema & Model

---
+++ Back-End/models/couponSchema.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+// Create Schema
+const couponSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "Coupon name required"],
+      unique: true,
+    },
+    expire: {
+      type: Date,
+      required: [true, "Coupon expire time required"],
+    },
+    discount: {
+      type: Number,
+      required: [true, "Coupon discount value required"],
+    },
+  },
+  { timestamps: true }
+);
+
+// Create model
+const couponModel = mongoose.model("Coupon", couponSchema);
+module.exports = BrandModel;
