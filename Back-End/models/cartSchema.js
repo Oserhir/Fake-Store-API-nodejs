f6fc1e240b1360cc4ffba06e963faaac9cb636f1
OSERHIRDEV <oserhir10@gmail.com>
2022-11-23 16:41:37 +0100

    Shopping Cart Schema & Model

---
+++ Back-End/models/cartSchema.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+// Create Schema
+const cartSchema = new mongoose.Schema(
+  {
+    cartItems: [
+      {
+        product: {
+          type: mongoose.Schema.ObjectId,
+          ref: "Product",
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+        color: String,
+        price: Number,
+      },
+    ],
+    totalCartPrice: Number,
+    totalPriceAfterDiscount: Number,
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+// Create model
+const BrandModel = mongoose.model("Cart", cartSchema);
+module.exports = BrandModel;
