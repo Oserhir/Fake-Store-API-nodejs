ab04657d89a9a9c0bb6020672f96fccab757ec3e
OSERHIRDEV <oserhir10@gmail.com>
2022-12-20 12:32:21 +0100

    Clear cart depend on cartId

--- Back-End/controllers/orderController.js
+++ Back-End/controllers/orderController.js
@@ -1,11 +1,13 @@
 const asyncHandler = require("express-async-handler");
 const ApiError = require("../utils/apiError");
 
-const Cart = require("../models/cartModel");
-const Order = require("../models/orderModel");
+const User = require("../models/userSchema");
+const Product = require("../models/productSchema");
+const Cart = require("../models/cartSchema");
+const Order = require("../models/orderSchema");
 
 // @desc    create cash order
-// @route   POST /api/orders/cartId
+// @route   POST /api/v1/orders/cartId
 // @access  Protected/User
 exports.createCashOrder = asyncHandler(async (req, res, next) => {
   // app settings
@@ -26,4 +28,28 @@ exports.createCashOrder = asyncHandler(async (req, res, next) => {
     : cart.totalCartPrice;
 
   const totalOrderPrice = cartPrice + taxPrice + shippingPrice;
+
+  // 3) Create order with default paymentMethodType cash
+  const order = await Order.create({
+    user: req.user._id,
+    cartItems: cart.cartItems,
+    shippingAddress: req.body.shippingAddress,
+    totalOrderPrice,
+  });
+
+  // 4) After creating order, decrement product quantity, increment product sold
+  if (order) {
+    const bulkOption = cart.cartItems.map((item) => ({
+      updateOne: {
+        filter: { _id: item.product },
+        update: { $inc: { quantity: -item.quantity, sold: +item.quantity } },
+      },
+    }));
+    await Product.bulkWrite(bulkOption, {});
+
+    // 5) Clear cart depend on cartId
+    await Cart.findByIdAndDelete(req.params.cartId);
+  }
+
+  res.status(201).json({ status: "success", data: order });
 });
